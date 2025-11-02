3f8d37165c26698c4f4e22d334f5833e977a015a
PriyankaRavee <priyankacsengr@gmail.com>
2025-11-03 02:02:43 +0530

    chnages

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,24 @@
 const express = require('express');
+const client = require('prom-client'); // Prometheus client
+
 const app = express();
-app.get('/api', (req, res) => res.send('Hello from backend!'));
-app.listen(3000, () => console.log('Backend running on port 3000'))
+const port = 3000;
+
+// Create a counter metric
+const requestCounter = new client.Counter({
+  name: 'nodejs_requests_total',
+  help: 'Total number of requests received'
+});
+
+app.get('/api', (req, res) => {
+  requestCounter.inc(); // Increment on each request
+  res.send('Hello from backend!');
+});
+
+// Prometheus metrics endpoint
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(await client.register.metrics());
+});
+
+app.listen(port, () => console.log(`Backend running on port ${port}`));
